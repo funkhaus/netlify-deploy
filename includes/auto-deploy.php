2580995a6e37710f62986d79db64ce40fddbaa9e
drewbaker <drewrb@gmail.com>
2020-11-18 15:53:40 +0100

    Update auto-deploy.php

--- includes/auto-deploy.php
+++ includes/auto-deploy.php
@@ -4,7 +4,7 @@
  */
     function nd_debounce_deploy($object_id) {
         $options = get_option('nd_settings');
-        
+
         // Abort early
         if( empty($options['auto_deploy']) || empty($options['build_hook_url']) ) {
             return false;
@@ -14,9 +14,9 @@
 
         if($deploy_time) {
             // Try again in 60 seconds
-            wp_clear_scheduled_hook('nd_run_auto_deploy')
+            wp_clear_scheduled_hook('nd_run_auto_deploy');
             wp_schedule_single_event($deploy_time, 'nd_run_auto_deploy');
-            
+
         } else {
             nd_deploy_post();
             set_transient('nd_deploy_time', time() + 59, 60);
@@ -27,12 +27,12 @@
     add_action('save_post', 'nd_debounce_deploy');
     add_action('attachment_updated', 'nd_debounce_deploy');
     add_action('nestedpages_post_order_updated', 'nd_debounce_deploy');
-    
-    
-    
+
+
+
 /*
  * Run an auto deploy
- */    
+ */
     function nd_run_auto_deploy() {
         nd_deploy_post();
     }
